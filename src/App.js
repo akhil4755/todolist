44e829817f6daaff39f755983ce5c592d2458621
akhil4755 <akhil4755@gmail.com>
2019-04-12 21:50:51 +0530

    added done state to the list items

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const initialstate = {
     list: [],
+    done: [],
 
-      textboxvalue: null,
+    textboxvalue: null,
   };
 
 class App extends React.Component {
@@ -12,70 +13,91 @@ class App extends React.Component {
     this.state = initialstate
 }
 
-handlecolorChange = (e) =>
+handleAddList = (e) =>
 {
-    let listitem = [{
+    let listitem = [{  item : this.state.textboxvalue }];
 
-      item : this.state.textboxvalue,      
-
-    }]
-
-    /*let flag=0;
-
-    if(this.state.customization.length === 0)
-    {
-      this.setState({ customization:customizationElement });
-    }
-    else 
-    {
-      let arr = this.state.customization;
-
-        for(let i=0; i<arr.length; i++)
-        {
-          if(arr[i].class === this.state.classValue )
-          {
-             arr[i].color = e.target.value;
-             this.setState({customization:arr})
-             flag = 1;
-          }
-        }
-        if(flag === 0)
-        {*/
-          let listarr = this.state.list;
-          let concatedcustarray = listarr.concat(listitem);
-          this.setState({list:concatedcustarray})
-        //}
-    //}
+    let listarr = this.state.list;
+    let concatedlistarray = listarr.concat(listitem);
+    this.setState({list:concatedlistarray})
 }
 
-handleDeleteitem = (e) =>
+handleDeletelistitem = (e) =>
 {
   let arr = this.state.list;
   arr.splice(e.target.id,1);
   this.setState({list: arr});
 }
 
+handleDeletedoneitem = (e) =>
+{
+  let arr = this.state.done;
+  arr.splice(e.target.id,1);
+  this.setState({done: arr});
+}
+
+handlechangetodone = (e) =>
+{
+  let arrList = this.state.list;
+  let arrDone = this.state.done;
+  let toDone = arrList[e.target.id];
+  arrList.splice(e.target.id, 1);
+  arrDone = arrDone.concat(toDone);
+  this.setState({done: arrDone});
+}
+
+handlechangebacktolist = (e) =>
+{  
+  let arrList = this.state.list;
+  let arrDone = this.state.done;
+
+  let toList = arrDone[e.target.id];
+  arrDone.splice(e.target.id, 1);
+
+  arrList = arrList.concat(toList);
+  this.setState({list: arrList});
+}
 
   render() {
-      console.log(this.state.list)
+      console.log("list" , this.state.list)
+      console.log("done" , this.state.done)
 
       let showlist = this.state.list.map( (value,i) => {
-          return(<div>
-              <p> {value.item} </p> 
-              <button id={i} onClick={this.handleDeleteitem}> X </button>
-          </div>)
+        return(<div>              
+                  <input id={i} type="checkbox" onChange={this.handlechangetodone}/>
+                  <input type="text" value={value.item}  onChange={(e)=> 
+                    { 
+                      let x = this.state.list;
+                      x[i].item = e.target.value;
+                      this.setState({list: x}) 
+                  }}/>
+                  <button id={i} onClick={this.handleDeletelistitem}> X </button>
+              </div>)
+      })
+
+      let done = this.state.done.map( (value,i) =>{
+        return(<div>
+                <input id={i} type="checkbox" onChange={this.handlechangebacktolist} checked/>
+                  <p> <s>{value.item} </s> </p>
+                  <button id={i} onClick={this.handleDeletedoneitem}> X </button>
+              </div>)
       })
     
     return (
         <div>
           <center> 
 
-            <h3> To Do's ! </h3> <br/>
+            <h3><u> To Do's ! </u></h3> <br/>
             
             { showlist }
+            <hr/>
+
+            { done }
+
+            <hr/>
 
             <input type="text" onChange={(e)=> { this.setState({textboxvalue:e.target.value}) }} />   
-            <button onClick={this.handlecolorChange} > add </button>
+            <button onClick={this.handleAddList} > add </button>
 
           </center>
         </div>
