ca3afefce682df8077668f9ab30a87a22226deb1
akhil4755 <akhil4755@gmail.com>
2019-04-14 00:57:33 +0530

    tried to switch the method of deleting elements

--- src/App.js
+++ src/App.js
@@ -5,55 +5,103 @@ const initialstate = {
     done: [],
 
     textboxvalue: null,
+    selectid:0,
+    count:0,
   };
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = initialstate
+    this.state = initialstate;
 }
 
+//Add elements to the list 
 handleAddList = (e) =>
 {
-    let listitem = [{  item : this.state.textboxvalue }];
-
     let listarr = this.state.list;
+
+    let listitem = [{  
+        item : this.state.textboxvalue, 
+        id : this.state.count 
+      }];
+
     let concatedlistarray = listarr.concat(listitem);
-    this.setState({list:concatedlistarray})
+    this.setState({ list:concatedlistarray })
+    let c = this.state.count; 
+    this.setState({ count: ++c });
 }
 
+//Delete element from unfinished items list
 handleDeletelistitem = (e) =>
 {
   let arr = this.state.list;
-  arr.splice(e.target.id,1);
+  let pos = null; 
+
+  for(let i=0; i<arr.length; i++)
+  {
+    if(arr[i].id === e.target.id)
+    {
+      pos = i;
+    }
+  }
+  arr.splice(pos,1);
   this.setState({list: arr});
 }
 
+//Delete element from done list
 handleDeletedoneitem = (e) =>
 {
   let arr = this.state.done;
-  arr.splice(e.target.id,1);
+  let pos = null;
+
+  for(let i=0; i<arr.length; i++)
+  {
+    if(arr[i].id === e.target.id)
+    {
+      pos = i;
+    }
+  }
+  arr.splice(pos,1);
   this.setState({done: arr});
 }
 
+//Moving list elements from unfinished to completed items
 handlechangetodone = (e) =>
 {
   let arrList = this.state.list;
   let arrDone = this.state.done;
-  let toDone = arrList[e.target.id];
-  arrList.splice(e.target.id, 1);
+  let pos = null;
+
+  for(let i=0; i< arrList.length; i++)
+  {
+    if(arrList[i].id === e.target.id)
+    {
+      console.log("jajaja");
+      pos = i;
+    }
+  }
+  let toDone = arrList[pos];
+  arrList.splice(pos, 1);
   arrDone = arrDone.concat(toDone);
   this.setState({done: arrDone});
 }
 
+//Moving list elements from done, back to yet to be completed
 handlechangebacktolist = (e) =>
 {  
-  let arrList = this.state.list;
   let arrDone = this.state.done;
+  let arrList = this.state.list;
+  let pos = null;
 
-  let toList = arrDone[e.target.id];
-  arrDone.splice(e.target.id, 1);
-
+  for(let i=0; i<arrDone.length; i++)
+  {
+    if(arrDone[i].id === e.target.id)
+    {
+      pos = i;
+    }
+  }
+  let toList = arrDone[pos];
+  arrDone.splice(pos, 1);
   arrList = arrList.concat(toList);
   this.setState({list: arrList});
 }
@@ -62,40 +110,69 @@ handlechangebacktolist = (e) =>
       console.log("list" , this.state.list)
       console.log("done" , this.state.done)
 
-      let showlist = this.state.list.map( (value,i) => {
-        return(<div>              
-                  <input id={i} type="checkbox" onChange={this.handlechangetodone}/>
-                  <input type="text" value={value.item}  onChange={(e)=> 
-                    { 
-                      let x = this.state.list;
-                      x[i].item = e.target.value;
-                      this.setState({list: x}) 
-                  }}/>
-                  <button id={i} onClick={this.handleDeletelistitem}> X </button>
-              </div>)
-      })
+//Print the items that are unfinished
+      let showlist = null;
 
-      let done = this.state.done.map( (value,i) =>{
-        return(<div>
-                <input id={i} type="checkbox" onChange={this.handlechangebacktolist} checked/>
-                  <p> <s>{value.item} </s> </p>
-                  <button id={i} onClick={this.handleDeletedoneitem}> X </button>
-              </div>)
-      })
+      if(this.state.list.length === 0)
+      {
+        showlist = null;
+      }
+      else
+      {
+        showlist = this.state.list.map( (value,i) => {
+          return(<div>              
+                    <input id={value.id} type="checkbox" onChange={this.handlechangetodone} />
+                    <input id={value.id} type="text" value={value.item}  onChange={(e)=> 
+                      { 
+                        let arr = this.state.list;
+                        let pos = null;
+                        for(let i=0; i<arr.length; i++)
+                        {
+                          if(arr[i].id === e.target.id)
+                            pos = i;
+                        }
+                        arr[pos].item = e.target.value;
+                        this.setState({list: arr}); 
+                    }}/>
+                    <button id={value.id} onClick={this.handleDeletelistitem}> X </button>
+                </div>)         
+        })
+      }
+
+//Print the items that are done
+      let done = null;
+
+      if(this.state.done.length === 0)
+      {
+        done = null;
+      }
+      else
+      {      
+        done = this.state.done.map((value) =>{
+          return(<div>
+                  <input id={value.id} type="checkbox" onChange={this.handlechangebacktolist} checked />
+                    <p> <s>{value.item} </s> </p>
+                    <button id={value.id} onClick={this.handleDeletedoneitem}> X </button>
+                </div>)
+        })
+      }
+
+//Separating the items based on completion      
+      let splitter = null;
+      if(this.state.done.length !==0)
+        splitter = (<hr/>)
     
+//Rendering
     return (
         <div>
           <center> 
 
             <h3><u> To Do's ! </u></h3> <br/>
             
-            { showlist }
+            { showlist }            
             <hr/>
-
             { done }
-
-            <hr/>
-
+            { splitter }
             <input type="text" onChange={(e)=> { this.setState({textboxvalue:e.target.value}) }} />   
             <button onClick={this.handleAddList} > add </button>
 
